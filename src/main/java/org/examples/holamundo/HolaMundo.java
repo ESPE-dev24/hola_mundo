392910d661ea77d680c3ac75663a678dc3d0aae7
Luis Zapata <jlzapata4@espe.edu.ec>
2024-11-22 20:44:36 -0500

    22/11/2024 - 20:44h 'Primer commit en Java'

---
+++ src/main/java/org/examples/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.examples.holamundo;
+
+/**
+ *
+ * @author Jose Luis Zapata
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
